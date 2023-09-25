17c5d5fa11e64b6383a3ac0c05354064f12041e3
Nuzul Abatony <albtony@gmail.com>
2023-09-25 23:35:24 +0700

    wip: basic structure done

---
+++ resources/views/form.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <header>
+        <h1>This is a form</h1>
+    </header>
+    <main>
+        <form>
+            <p>form</p>
+        </form>
+    </main>
+</body>
+</html>
